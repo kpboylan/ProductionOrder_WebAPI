f807ae8a96b328be8da59a11880a0febf5d470ae
kpboylan <kboylan@gmail.com>
2024-06-23 16:55:25 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+using ProductionOrder_WebAPI.DAL;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddCors(p => p.AddPolicy("corsapp", builder =>
+{
+    builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
+}));
+
+builder.Services.AddScoped<IProdOrderRepository, ProdOrderRepository>();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors("corsapp");
+
+app.UseHttpsRedirection();
+
+app.UsePathBase("/api");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
