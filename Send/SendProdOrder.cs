21242dd5001eb806236f0eb2037a09aaf5d296f5
kpboylan <kboylan@gmail.com>
2024-06-23 16:54:29 +0100

    Add files via upload

---
+++ Send/SendProdOrder.cs
@@ -0,0 +1,34 @@
+﻿using ProductionOrder_WebAPI.Model;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace ProductionOrder_WebAPI.Send
+{
+    public class SendProdOrder
+    {
+        public string Send(Order prodOrder)
+        {
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.QueueDeclare(queue: "ProdOrder",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+            var json = JsonConvert.SerializeObject(prodOrder);
+
+            var body = Encoding.UTF8.GetBytes(json);
+
+            channel.BasicPublish(exchange: string.Empty,
+                                 routingKey: "ProdOrder",
+                                 basicProperties: null,
+                                 body: body);
+
+            return "Success";
+        }
+    }
+}
